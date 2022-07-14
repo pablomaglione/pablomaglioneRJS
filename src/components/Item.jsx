7a18e8d0916cf392a9779b41dfca6e73a76c46fe
pablomaglione <pmaglione83@gmail.com>
2022-07-14 19:27:36 -0300

    desafio 5 - diseño card

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,17 @@
 const Item = ({ producto }) => {
-    const { nombre, detalle, precio, img } = producto;
+    const { nombre, detalle, precio, img, id } = producto;
     return (
-        <div className="card d-flex" style={{ width: "15rem" }}>
-             <img className="card-img-top" src={img} alt="Card image cap" />
-            <div className="card-body d-flex flex-column justify-content-center">
-                <h3 className="card-title">{nombre}</h3>
-                <p className="card-title">{detalle}</p>
-                <p className="card-text">{`$${precio}`}</p>
-                <a href="#" className="btn btn-primary">Agregar Producto</a>
+        <div className="card rounded border-0" style={{ width: "15rem" }}>
+            <div className="card-body p-4 d-flex flex-column justify-content-center">
+                <img className="card-img-top" src={img} alt="Card image cap"/>
+                <h2 className="card-title text-center">{nombre}</h2>
+                <p className="card-detalle small text-center text-muted font-italic">{detalle}</p>
+                <p className="card-precio text-center card-text">{`$${precio}`}</p>
+                <button id={id} className="third add btn-agregar btn btn-primary">Agregar Producto<i aria-hidden="true"></i></button>
             </div>
         </div>
     );
 };
 
 export default Item;
+
